c1813e3cee72de118a033ca23267c0c9246ff25e
Jakub Darul <daruljakub@gmail.com>
2019-03-25 21:46:23 +0100

    started working on lab3

--- lab2/import.py
+++ lab2/import.py
@@ -6,11 +6,11 @@ es = Elasticsearch(retry_on_timeout=True)
 actions = [{
     "_index": "law_index6",
     "_id": i,
-    "_type":"_doc",
+    "_type": "_doc",
     "_source": {
-        "content": open("ustawy/" + file).read(),
+        "content": open("../ustawy/" + file).read(),
         "filename": file
     }
-} for i, file in enumerate(os.listdir("ustawy"))]
+} for i, file in enumerate(os.listdir("../ustawy"))]
 
 helpers.bulk(es, actions)
