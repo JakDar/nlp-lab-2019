cdffb06a8591932ed58c0bffc9c8f93d4a9681a7
Jakub Darul <daruljakub@gmail.com>
2019-05-27 16:09:56 +0200

    ???

--- lab10/tmp.py
+++ lab10/tmp.py
@@ -34,25 +34,33 @@ def count_subseries(subsequence: List[int], input_list: List[int]):
 
 
 def remove_repeating_subseqs(subseq: List[int], input_list: List[int]):
+    end = end_of_subseq(subseq, input_list)
+
+    rest = input_list[end + 1:]
+
+    subseq_str = seq(subseq).reduce(lambda x, y: f"{x},{y}")
+    end_str = seq(rest).reduce(lambda x, y: f"{x},{y}")
+
+    new_end = end_str.replace(subseq_str,"").replae(",,",",")
+
+
+
+
+    return rest
+
+
+def end_of_subseq(subseq: List[int], input_list: List[int]):
     found_first = False
     seq_i = 0
     result = []
-    for entry in input_list:
+    for i, entry in enumerate(input_list):
         if subseq[seq_i] == entry:
             seq_i += 1
         else:
             seq_i = 0
 
         if seq_i == len(subseq):
-            if found_first:
-                result = result[len(subseq) - 1:]
-                seq_i = 0
-                continue
-            else:
-                found_first = True
-                seq_i = 0
-        result.append(entry)
-    return result
+            return i
 
 
 if __name__ == '__main__':
